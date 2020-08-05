f89190886126cc26f8daf77326f5a127e22b19ee
Miao ZhiCheng <miao@decentral.ee>
2020-08-05 17:51:19 +0300

    updated demo

--- contracts/Simple.sol
+++ contracts/Simple.sol
@@ -14,6 +14,20 @@ contract A {
         revert("I am broken");
     }
 
+    function doEmptyRevert() external {
+        intestant.push(2);
+        revert();
+    }
+
+    function doAssert() external {
+        intestant.push(2);
+        assert(false);
+    }
+
+    function doDivideByZero() external {
+        uint a = uint(1) / uint(0);
+    }
+
     function useAllGas() external {
         while (true) intestant.push(3);
     }
@@ -40,13 +54,37 @@ contract A {
 contract SimpleTester {
     A a;
 
+    uint256 constant GAS_RESERVATION = 10000;
+
+    event Result(string result, bytes reason, uint256 gasLeft);
+
     constructor() public {
         a = new A();
     }
 
-    function case1() external {
-        //uint256 gasLeft = gasleft();
-        try a.doSomething() {
+    function _callCallback(bytes memory data) private {
+        uint256 gasLeft = gasleft();
+        uint256 gasBudget = gasLeft - GAS_RESERVATION;
+        (bool success, bytes memory returnedData) =
+            address(a).call{gas: gasBudget}(data);
+        if (success) {
+            emit Result("success", "", gasleft());
+        } else {
+            gasLeft = gasleft();
+            emit Result("revert1", returnedData, gasleft());
+            if (gasLeft < GAS_RESERVATION) {
+                // this is out of gas, but the call may still fail if more gas is provied
+                // and this is okay, because there can be incentive to jail the app by providing
+                // more gas
+            } else {
+                // go to jail
+            }
+        }
+    }
+
+    function case1_v1() external {
+        uint256 gasLeft = gasleft();
+        try a.doSomething{gas: gasLeft - GAS_RESERVATION}() {
         } catch Error(string memory reason) {
             require(false, reason);
         } catch (bytes memory lowLevelData) {
@@ -56,17 +94,70 @@ contract SimpleTester {
             /* if (isOutOfGas(lowLevelData)) {
                 // ... do not send app to jail
             } */
-            require(false, "Low level data caught");
+            emit Result("revert2", lowLevelData, gasleft());
+            return;
         }
+        emit Result("success", "", gasleft());
     }
 
-    function case2() external {
-        bool caught = false;
-        try a.doRevert() {
-        } catch {
-            caught = true;
+    function case1() external {
+        _callCallback(abi.encodeWithSelector(A.doSomething.selector));
+    }
+
+    function case2_Revert() external {
+        _callCallback(abi.encodeWithSelector(A.doRevert.selector));
+        /* uint256 gasLeft = gasleft();
+        try a.doRevert{gas: gasLeft - GAS_RESERVATION}() {
+        } catch Error(string memory reason) {
+            emit Result("revert1", bytes(reason), gasleft());
+            return;
+        } catch (bytes memory lowLevelData) {
+        emit Result("revert2", lowLevelData, gasleft());
+            return;
         }
-        require(caught, "Something should be caught");
+        revert("Something should be caught"); */
+    }
+
+    function case2_EmptyRevert() external {
+        _callCallback(abi.encodeWithSelector(A.doEmptyRevert.selector));
+        /* uint256 gasLeft = gasleft();
+        try a.doEmptyRevert{gas: gasLeft - GAS_RESERVATION}() {
+        }  catch Error(string memory reason) {
+            emit Result("revert1", bytes(reason), gasleft());
+            return;
+        } catch (bytes memory lowLevelData) {
+        emit Result("revert2", lowLevelData, gasleft());
+            return;
+        }
+        revert("Something should be caught"); */
+    }
+
+    function case2_Assert() external {
+        _callCallback(abi.encodeWithSelector(A.doAssert.selector));
+        /* uint256 gasLeft = gasleft();
+        try a.doAssert{gas: gasLeft - GAS_RESERVATION}() {
+        }  catch Error(string memory reason) {
+            emit Result("revert1", bytes(reason), gasleft());
+            return;
+        } catch (bytes memory lowLevelData) {
+        emit Result("revert2", lowLevelData, gasleft());
+            return;
+        }
+        revert("Something should be caught"); */
+    }
+
+    function case2_DivideByZero() external {
+        _callCallback(abi.encodeWithSelector(A.doDivideByZero.selector));
+        /* uint256 gasLeft = gasleft();
+        try a.doDivideByZero{gas: gasLeft - GAS_RESERVATION}() {
+        }  catch Error(string memory reason) {
+            emit Result("revert1", bytes(reason), gasleft());
+            return;
+        } catch (bytes memory lowLevelData) {
+        emit Result("revert2", lowLevelData, gasleft());
+            return;
+        }
+        revert("Something should be caught"); */
     }
 
     function case3() external {
@@ -74,7 +165,7 @@ contract SimpleTester {
         try a.useAllGas() {
         } catch Error(string memory reason) {
             require(false, reason);
-        } catch (bytes memory lowLevelData) {
+        } catch (bytes memory) {
             /* if (isOutOfGas(lowLevelData)) {
                 // ... do not send app to jail
             } */
--- test/simple.js
+++ test/simple.js
@@ -15,24 +15,91 @@ contract("Test simple", (accounts) => {
     });
 
     it("case1 - normal gas limit should not be treated as errors", async () => {
-        await web3tx(tester.case1, "tester.case1 pass 1")();
-        await web3tx(tester.case1, "tester.case1 pass 2")();
-        const tx = await web3tx(tester.case1, "tester.case1 pass 3")();
-        await web3tx(tester.case1, "tester.case1 - with less gas")({
+        let tx;
+        let gasEstimation;
+
+        tx = await web3tx(tester.case1, "tester.case1 pass 1")();
+        assert.equal(tx.logs[0].event, "Result");
+        assert.equal(tx.logs[0].args.result, "success");
+        assert.equal(tx.logs[0].args.reason, null);
+
+        tx = await web3tx(tester.case1, "tester.case1 pass 2")();
+        assert.equal(tx.logs[0].args.result, "success");
+        assert.equal(tx.logs[0].args.reason, null);
+
+        tx = await web3tx(tester.case1, "tester.case1 pass 3")();
+        assert.equal(tx.logs[0].args.result, "success");
+        assert.equal(tx.logs[0].args.reason, null);
+
+        gasEstimation = await tester.case1.estimateGas();
+        console.log("gas used by last tx", tx.receipt.gasUsed);
+        console.log("gasEstimation", gasEstimation);
+        tx = await web3tx(tester.case1, "tester.case1 - with less gas")({
             gas: tx.receipt.gasUsed
         });
+        assert.equal(tx.logs[0].args.gasLeft.toString(), "7454");
+        assert.equal(tx.logs[0].args.reason, null);
+        assert.equal(tx.logs[0].args.result, "revert2");
     });
 
-    it("case2 - catch normal errors", async () => {
-        await web3tx(tester.case2, "tester.case2")();
+    it("case2 - catch errors", async () => {
+        let tx;
+
+        tx = await web3tx(tester.case2_EmptyRevert, "tester.case2_Revert")();
+        assert.equal(tx.logs[0].args.result, "revert1");
+        assert.equal(tx.logs[0].args.reason, "0x" + new Buffer("I am broken").toString("hex"));
+
+        tx = await web3tx(tester.case2_Revert, "tester.case2_Revert - with less gas")({
+            gas: tx.receipt.gasUsed - 5000
+        });
+        assert.equal(tx.logs[0].args.reason, null);
+        assert.equal(tx.logs[0].args.result, "revert2");
+
+        tx = await web3tx(tester.case2_DivideByZero, "tester.doDivideByZero")();
+        assert.equal(tx.logs[0].args.reason, null);
+        assert.equal(tx.logs[0].args.result, "revert1");
+        tx = await web3tx(tester.case2_DivideByZero, "tester.doDivideByZero - with less gas")({
+            gas: tx.receipt.gasUsed - 5000
+        });
+        assert.equal(tx.logs[0].args.result, "revert2");
+        assert.equal(tx.logs[0].args.reason, "");
     });
+
+    it.only("gas usage printouts", async () => {
+        let tx;
+        const printTx = (tx) => {
+            console.log(tx.logs[0].args.result, tx.logs[0].args.reason, tx.logs[0].args.gasLeft.toString());
+            console.log("=".repeat(80));
+        }
+
+        tx = await web3tx(tester.case1, "tester.case1 pass1")();
+        printTx(tx);
+        tx = await web3tx(tester.case1, "tester.case1 pass2")();
+        printTx(tx);
+        tx = await web3tx(tester.case1, "tester.case1 pass3 limited gas")({ gas: tx.receipt.gasUsed });
+        printTx(tx);
+
+        tx = await web3tx(tester.case2_Revert, "tester.case2_Revert pass1")();
+        printTx(tx);
+
+        tx = await web3tx(tester.case2_EmptyRevert, "tester.case2_EmptyRevert pass1")();
+        printTx(tx);
+        tx = await web3tx(tester.case2_EmptyRevert, "tester.case2_EmptyRevert pass2 limited gas")({ gas: tx.receipt.gasUsed });
+        printTx(tx);
+
+        tx = await web3tx(tester.case2_Assert, "tester.case2_EmptyRevert")();
+        printTx(tx);
+
+        tx = await web3tx(tester.case2_DivideByZero, "tester.case2_DivideByZero")();
+        printTx(tx);
+    })
 
     it("case3 - catch DoS by inducing out of gas", async () => {
         let tx = await web3tx(tester.case3, "tester.case3")();
-        console.log("tx !!!!", wad4human(tx.logs[0].args.amount), tx.logs[0].args.amount);
+        console.log("tx", wad4human(tx.logs[0].args.amount), tx.logs[0].args.amount);
     });
 
-    it.only("case3 and case 4 some magic", async () => {
+    it("case3 and case 4 some magic", async () => {
         let tx1 = await web3tx(tester.case4, "tester.case4")();
         console.log("gas cost1", tx1.logs[0].args.amount.toString());
         let tx2 = await web3tx(tester.case5, "tester.case5")();
